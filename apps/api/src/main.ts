9cca285d5613e3befe802c3b1786f31de3eedc48
DanielFlores1223 <dany-232000@hotmail.com>
2024-08-30 10:36:51 -0600

    swagger installed and implemented in main file

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ResponseInterceptor } from './common/interceptions';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -16,6 +17,20 @@ async function bootstrap() {
   );
 
   app.useGlobalInterceptors(new ResponseInterceptor());
+
+  const config = new DocumentBuilder()
+    .setTitle('DocumForm API')
+    .setDescription(`
+        The Docuform API is a powerful and intuitive RESTful service designed to convert PDF 
+        documents into dynamic, fully customizable forms. It enables users to quickly extract data and 
+        create forms that meet specific needs with ease. The API is built to be flexible, scalable, and 
+        easy to integrate into various applications.
+    `)
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docuform/api', app, document);
 
   await app.listen(process.env.PORT);
 
