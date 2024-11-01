103095b47cee4b8d767ecd1a27ba4134afed5128
DanielFlores1223 <dany-232000@hotmail.com>
2024-10-31 22:23:37 -0600

    login layou and design were done!

--- apps/client/src/app/auth/layouts/auth-layout/auth-layout.component.ts
+++ apps/client/src/app/auth/layouts/auth-layout/auth-layout.component.ts
@@ -3,17 +3,8 @@ import { ResponsiveService } from '../../../material/services/responsive.service
 
 @Component({
   templateUrl: './auth-layout.component.html',
-  styleUrl: './auth-layout.component.css'
+  styleUrl: './auth-layout.component.css',
 })
 export class AuthLayoutComponent {
-  tiles: any[] = [
-    {text: 'One', cols: 3, rows: 1, color: 'lightblue'},
-    {text: 'Two', cols: 1, rows: 2, color: 'lightgreen'},
-    {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
-    {text: 'Four', cols: 2, rows: 1, color: '#DDBDF1'},
-  ];
-
   public layout = inject(ResponsiveService);
-
-
 }
