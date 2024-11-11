024c16f3155fb7722ceee79d0b26a0370dabeac9
DanielFlores1223 <dany-232000@hotmail.com>
2024-11-11 00:13:08 -0600

    dynamic form preview design created

--- apps/client/src/app/dynamic-form/pages/create-page/create-page.component.ts
+++ apps/client/src/app/dynamic-form/pages/create-page/create-page.component.ts
@@ -1,16 +1,25 @@
-import { AfterViewInit, Component, HostListener, OnInit, computed, inject, signal } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  HostListener,
+  OnInit,
+  computed,
+  inject,
+  signal,
+} from '@angular/core';
 import { ResponsiveService } from '../../../material/services/responsive.service';
+import { FormFieldsPayload } from 'global-interfaces';
 
 @Component({
   selector: 'dynamic-form-create',
   templateUrl: './create-page.component.html',
-  styleUrl: './create-page.component.css'
+  styleUrl: './create-page.component.css',
 })
 export class CreatePageComponent implements AfterViewInit {
   public layout = inject(ResponsiveService);
   public innerHeight = signal<number | null>(null);
-  public maxHeighFieldsDiv = computed(() => {
-    if(!this.innerHeight()) return 0;
+  public maxHeighFieldsDiv = computed((): string => {
+    if (!this.innerHeight()) return 'max-height: 100vh';
 
     const px = this.innerHeight()! * 0.3;
 
@@ -18,21 +27,42 @@ export class CreatePageComponent implements AfterViewInit {
   });
 
   public rowHeightScreen = computed(() => {
-    if(!this.innerHeight() || ( this.innerHeight()! && this.innerHeight()! > this.layout.MEDIUM_HEIGHT ))
-        return '100vh'
+    if (
+      !this.innerHeight() ||
+      (this.innerHeight()! && this.innerHeight()! > this.layout.MEDIUM_HEIGHT)
+    )
+      return '100vh';
 
     let screenPorcentagePx: number = this.innerHeight()! * 0.2;
 
-    if(this.innerHeight()! <= this.layout.SMALL_HEIGHT)
+    if (this.innerHeight()! <= this.layout.SMALL_HEIGHT)
       screenPorcentagePx = this.innerHeight()! * 0.3;
 
     const totalPxScreen = this.innerHeight()! + screenPorcentagePx;
 
-    return `${totalPxScreen}px`
+    return `${totalPxScreen}px`;
   });
 
+  public formFields: FormFieldsPayload[] = [
+    {
+      name: 'form1',
+      idFieldType: 1,
+      scannedDocumentSeparator: '1',
+    },
+    {
+      name: 'form2',
+      idFieldType: 2,
+      scannedDocumentSeparator: '2',
+    },
+    {
+      name: 'form3',
+      idFieldType: 1,
+      scannedDocumentSeparator: '3',
+    },
+  ];
+
   ngAfterViewInit(): void {
-   this.innerHeight.set(window.innerHeight);
+    this.innerHeight.set(window.innerHeight);
   }
 
   @HostListener('window:resize', ['$event'])
@@ -40,5 +70,4 @@ export class CreatePageComponent implements AfterViewInit {
     const innerHeight = (event.target as Window).innerHeight;
     this.innerHeight.set(innerHeight);
   }
-
 }
