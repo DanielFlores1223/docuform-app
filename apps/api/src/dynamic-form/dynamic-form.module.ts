8a1a2dced3e0fb63ce83ba0a5bf340bf8721c3e4
DanielFlores1223 <dany-232000@hotmail.com>
2025-05-20 20:43:07 -0600

    Apply SRP constraint validations dynamic form

--- apps/api/src/dynamic-form/dynamic-form.module.ts
+++ apps/api/src/dynamic-form/dynamic-form.module.ts
@@ -5,10 +5,17 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { DynamicForm, FieldType, FormField } from './entities';
 import { SlugService } from 'src/common/services';
 import { OwnerDynamicFormGuard } from './guards/';
+import { DynamicFormDBValidatorService } from './services'
 
 @Module({
   imports: [ TypeOrmModule.forFeature([DynamicForm, FormField, FieldType]) ],
   controllers: [DynamicFormController],
-  providers: [DynamicFormService, TypeOrmModule, SlugService, OwnerDynamicFormGuard],
+  providers: [
+    DynamicFormService, 
+    TypeOrmModule, 
+    SlugService, 
+    OwnerDynamicFormGuard, 
+    DynamicFormDBValidatorService
+  ],
 })
 export class DynamicFormModule {}
